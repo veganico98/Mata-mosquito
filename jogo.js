97a3b8d73a16ecc885bb43287e856c6efd73b73b
Vega <125098430+veganico98@users.noreply.github.com>
2024-12-10 16:10:23 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -1,5 +1,20 @@
 var altura = 0
 var largura = 0
+var vidas = 1
+var tempo = 10
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search;
+nivel = nivel.replace('?', '');
+
+if(nivel === 'normal'){
+    criaMosquitoTempo = 1500
+}else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+}else if(nivel === 'veryHard'){
+    criaMosquitoTempo = 750
+}
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -10,10 +25,35 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+    tempo -= 1
+
+    if(tempo<0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert('vitoria') 
+        window.location.href = "vitoria.html"
+    }else{
+    document.getElementById('cronometro').innerHTML = tempo
+    }
+}, 1000)
+
 function posicaoRandomica(){
     if(document.getElementById('mosquitao')){
     document.getElementById('mosquitao').remove()
+    
+
+    if(vidas > 3){
+        window.location.href = 'fim_de_jogo.html'
+    }else{
+        document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+
+        vidas++
     }
+}
+        
+    
+
     
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -29,7 +69,10 @@ function posicaoRandomica(){
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
-    mosquito.id = 'mosquitao'
+    mosquito.id = 'mosquitao' 
+    mosquito.onclick = function() {
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
 
